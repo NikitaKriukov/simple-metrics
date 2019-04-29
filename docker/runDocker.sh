cefd91930bcdb48912ba3f38e180e36ff970d4e0
nikita Kriukov <nikita.krjukov@gmail.com>
2019-04-29 14:49:10 -0700

    correct docker scripts

--- docker/runDocker.sh
+++ docker/runDocker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run --name aaa --rm -p 8080:8080 -it temp/nikita_monitoring:1.0 bash -c "cd /appl && ls -la && ./start.sh"
+docker run --name aaa -p 8080:8080 --rm -it temp/nikita_monitoring:1.0
